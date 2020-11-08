dc522f9d102074268a2cb3956df0e9ed4bbe898d
Antoine <achatain@users.noreply.github.com>
2020-11-08 01:17:25 +0000

    Set test up with fixture creating tmp files

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
+import os
+
+import pytest
+
 from dupekiller import files
 
+TMP_FILES_NUM = 2
 
-def test_walk() -> None:
-    assert len(files.walk("./tests")) == 2
+
+@pytest.fixture
+def tmp_files(tmp_path):
+    for index in range(TMP_FILES_NUM):
+        with open(os.path.join(tmp_path, f"file_{index}"), "x") as file:
+            file.write(str(index))
+    return tmp_path
+
+
+def test_walk(tmp_files) -> None:
+    print(tmp_files)
+    assert len(files.walk(str(tmp_files))) == TMP_FILES_NUM
